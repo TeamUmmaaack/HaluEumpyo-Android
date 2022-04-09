c867ad02dcfd1604013516ce0d9905b90ee49e7a
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-04-10 01:41:54 +0900

    [ADD] 모아보기 뷰 어댑터 연결

--- app/src/main/java/com/ummaaack/halueumpyo/presentation/ui/seeall/SeeAllActivity.kt
+++ app/src/main/java/com/ummaaack/halueumpyo/presentation/ui/seeall/SeeAllActivity.kt
@@ -1,16 +1,119 @@
 package com.ummaaack.halueumpyo.presentation.ui.seeall
 
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.NumberPicker
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ummaaack.halueumpyo.R
 import com.ummaaack.halueumpyo.databinding.ActivitySeeAllBinding
 import com.ummaaack.halueumpyo.presentation.base.BaseViewUtil
+import com.ummaaack.halueumpyo.presentation.ui.MainActivity
+import org.cardna.presentation.ui.alarm.adapter.DiaryAdapter
+import org.cardna.presentation.ui.alarm.adapter.DiaryResponseData
+import java.util.*
 
 class SeeAllActivity : BaseViewUtil.BaseAppCompatActivity<ActivitySeeAllBinding>(R.layout.activity_see_all) {
+
+    private lateinit var diaryAdapter: DiaryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
     }
 
     override fun initView() {
+        setFriendRequestAdapter()
+        setDatePickerClickListener()
+    }
+
+
+    private fun setFriendRequestAdapter() {
+        val dataList = mutableListOf(
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+            DiaryResponseData("4", "WED", "오늘 먹은 음식은 가나다라마사다다리ㅏ너아ㅓㅏㄹ어ㅏ러ㅏ러아러ㅏ어라ㅓ라아러ㅏ어라ㅓㅇ","한숨","이하이"),
+        )
+
+        diaryAdapter = DiaryAdapter { item ->
+          startActivity(Intent(this, MainActivity::class.java))
+        }
+        with(binding.rvSeeAll) {
+            adapter = diaryAdapter
+            layoutManager = LinearLayoutManager(this@SeeAllActivity)
+
+            diaryAdapter.submitList(dataList)
+        }
+    }
+
+
+    private fun setDatePickerClickListener() {
+
+
+        //  날짜 dialog
+        binding.ivSeeAllDatePicker.setOnClickListener {
+
+            val dialog = AlertDialog.Builder(this).create()
+            val edialog = LayoutInflater.from(this)
+            val mView = edialog.inflate(R.layout.dialog_date_picker, null)
+
+            val year: NumberPicker = mView.findViewById(R.id.yearpicker_datepicker)
+            val month: NumberPicker = mView.findViewById(R.id.monthpicker_datepicker)
+            val cancel: Button = mView.findViewById(R.id.btn_dialog_date_picker_cancle)
+            val save: Button = mView.findViewById(R.id.btn_dialog_date_picker_confirm)
+
+
+            //  순환 안되게 막기
+            year.wrapSelectorWheel = false
+            month.wrapSelectorWheel = false
+
+            //  editText 설정 해제
+            year.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+            month.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+
+            //  최소값 설정
+            year.minValue = 2019
+            month.minValue = 1
+
+            //  최대값 설정
+            year.maxValue = 2021
+            month.maxValue = 12
+            year.displayedValues = arrayOf("2019년", "2020년", "2021년")
+            month.displayedValues = arrayOf("1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월")
+
+            //  취소 버튼 클릭 시
+            cancel.setOnClickListener {
+                dialog.dismiss()
+                dialog.cancel()
+            }
+
+            //  완료 버튼 클릭 시
+            save.setOnClickListener {
+                binding.tvSeeAllDate.text = (year.value).toString() + "년" + " " + (month.value).toString() + "월"
+
+                dialog.dismiss()
+                dialog.cancel()
+            }
+
+
+            dialog.setView(mView)
+            dialog.create()
+            dialog.show()
+
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/ummaaack/halueumpyo/presentation/ui/seeall/adapter/DiaryResponseData.kt
@@ -0,0 +1,11 @@
+package org.cardna.presentation.ui.alarm.adapter
+
+
+//TODO  서버연결 후 더미 리스트 삭제
+data class DiaryResponseData(
+    val month: String,
+    val day: String,
+    val content: String,
+    val musicName: String,
+    val musicSinger: String
+)
